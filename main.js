0e7f97f9df93510c49a842a5ecc4182384de743b
OkoloJohnbosco <chukwuemekaokolo233@gmail.com>
2023-05-01 00:21:03 +0100

    feat: finished modal

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-// Create a funtion to toggle the modal
+const openModalBtn = document.querySelector(".modal_open_btn");
+const closeModalBtn = document.querySelector(".modal_close_btn");
 
+openModalBtn.addEventListener("click", openModal)
+closeModalBtn.addEventListener("click", closeModal)
+
+// Create a funtion to open/close the modal
+function openModal() {
+    const modalController = new Modal("modal", "modal_overlay", "modal_content")
+    modalController.show()
+}
+
+function closeModal() {
+    const modalController = new Modal("modal", "modal_overlay", "modal_content")
+    modalController.hide()
+}
 
 class Modal {
-    constructor(modalClassName, modalOverlayClassName) {
+    constructor(modalClassName, modalOverlayClassName, modalContentClassName) {
         this.modal = document.querySelector(`.${modalClassName}`)
         this.modalOverLay = document.querySelector(`.${modalOverlayClassName}`)
+        this.modalContent = document.querySelector(`.${modalContentClassName}`)
+        this.pageBody = document.querySelector("body")
     }
 
-    logElement() {
-        console.log(this.modal)
+    hide() {
+        this.modalOverLay.classList.remove("shown")
+        this.modalOverLay.classList.add("fade")
+        this.modalContent.classList.remove("shown")
+        this.modalContent.classList.add("fade")
+        setTimeout(() => {
+            this.unMount()
+        }, 1000)
+    }
+
+    show() {
+        this.mount()
+        setTimeout(() => {
+            this.modalOverLay.classList.add("shown")
+            this.modalContent.classList.add("shown")
+        }, 100)
+    }
+
+    unMount() {
+        this.modal.style.display = "none"
+        this.pageBody.classList.remove("modal_shown")
+    }
+
+    mount() {
+        this.modal.style.display = "flex"
+        this.pageBody.classList.add("modal_shown")
+        this.modalOverLay.classList.remove("fade")
+        this.modalContent.classList.remove("fade")
     }
 }
